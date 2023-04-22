5f4a3b495f28da9016ce9bb50b42058c8531f7d4
Davod3 <davidpereira.pereira4@gmail.com>
2023-04-22 22:38:42 +0100

    Initial setup of authentication service

--- projetoPSI-angular/src/app/authentication.service.ts
+++ projetoPSI-angular/src/app/authentication.service.ts
@@ -1,11 +1,53 @@
 import { Injectable } from '@angular/core';
 import { User } from './user';
 import { Token } from '@angular/compiler';
+import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthenticationService {
 
-  constructor() { }
+  private token: string;
+
+  constructor(private http: HttpClient, private router: Router) {}
+
+  private saveToken(token: string): void {
+    localStorage.setItem('user-token', token);
+    this.token = token;
+  }
+
+  private getToken(): string {
+
+    if(!this.token){
+      this.token = localStorage.getItem('user-token');
+    }
+
+    return this.token;
+  }
+
+  public getUser(): User {
+    const token = this.getToken();
+
+    if(token){
+      let payload = token.split('.')[1];
+      payload = window.atob(payload);
+      return JSON.parse(payload);
+    } else {
+      return null;
+    }
+  }
+
+  public isLoggedIn(): boolean {
+
+    const user = this.getUser();
+
+    if(user) {
+      return user.exp > Date.now() / 1000;
+    } else {
+      return false;
+    }
+
+  }
 }
