0171d3c7f3dbe5f2cd22a0ce7f262887ab4c0f8c
Davod3 <davidpereira.pereira4@gmail.com>
2023-04-22 18:41:57 +0100

    Set up jwt in UserSchema

--- projetoPSI-express/models/users.js
+++ projetoPSI-express/models/users.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const jwt = require('jsonwebtoken');
 
 var UserSchema = new mongoose.Schema({
 
@@ -6,5 +7,20 @@ var UserSchema = new mongoose.Schema({
     password: {type: String, required: true}
 });
 
+UserSchema.methods.genJwt() = function() {
+
+    let expirationDate = new Date();
+    expirationDate.setDate(expirationDate.getDate() + 7);
+
+    return jwt.sign({
+        _id: this._id,
+        username: this.username,
+        password: this.password,
+        expires: parseInt(expirationDate.getTime() / 1000)
+    }, 'CASADOSSEGREDOS');
+
+};
+
 // Export model
 module.exports = mongoose.model("User", UserSchema);
+
