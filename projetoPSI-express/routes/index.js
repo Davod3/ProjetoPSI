6e51978008b0e3f90837a44553867090be97e7f2
Davod3 <davidpereira.pereira4@gmail.com>
2023-04-22 20:40:37 +0100

    Working register endpoint

--- projetoPSI-express/routes/index.js
+++ projetoPSI-express/routes/index.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var router = express.Router();
 
+const authentication_controller = require('../controllers/authController');
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+router.post('/authenticate', authentication_controller.register);
 
 module.exports = router;
