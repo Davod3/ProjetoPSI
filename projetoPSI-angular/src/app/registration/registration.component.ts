fa3a15189f9c8147c66380bade6d2d5c2e6727ae
Davod3 <davidpereira.pereira4@gmail.com>
2023-04-25 10:30:14 +0100

    Working input validation register frontend

--- projetoPSI-angular/src/app/registration/registration.component.ts
+++ projetoPSI-angular/src/app/registration/registration.component.ts
@@ -12,7 +12,10 @@ export class RegistrationComponent {
 
   credentials: UserToken = {
     username: '',
-    password: ''
+    password: '',
+    isValid: true,
+    err: ''
+
   };
 
   constructor(
@@ -23,9 +26,57 @@ export class RegistrationComponent {
   };
 
   register() {
-    this.authenticator.register(this.credentials).subscribe(() => {
-      console.log('User Registered');
-    });
+
+    this.credentials.err = '';
+    this.credentials.isValid = true;
+    const validPassword = new RegExp('^(?=.*[A-Z])(?=.*[a-z])(?=.*\d).+$');
+    const validUsername = new RegExp('^[a-zA-Z0-9]+$');
+
+    if(this.credentials.username.length < 3) {
+        this.credentials.isValid = false;
+        this.credentials.err+="Username must have more than 3 characters!";
+        this.credentials.err+="\n";
+    }
+
+    if(this.credentials.password.length < 8) {
+        this.credentials.isValid = false;
+        this.credentials.err+="Password must have more than 8 characters!";
+        this.credentials.err+="\n";
+    }
+
+    if(!validPassword.test(this.credentials.password)) {
+        this.credentials.isValid = false;
+        this.credentials.err+="Password must contain at least one Upper case letter, one Lower case letter and one number!";
+        this.credentials.err+="\n";
+    }
+
+    if(!validUsername.test(this.credentials.username)) {
+        this.credentials.isValid= false;
+        this.credentials.err+="Username must contain only alphanumeric characters!";
+        this.credentials.err+="\n";
+    }
+
+    if(this.credentials.isValid){
+
+      this.authenticator.register(this.credentials).subscribe((response) => {
+       
+        if(response.err){
+          
+          this.credentials.isValid = false;
+          this.credentials.err += response.err;
+
+        } else {
+
+          console.log("User registered!");
+
+          //Redirect to dashboard
+
+        }
+
+      });
+
+    }
+
   }
 
 }
