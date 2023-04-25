1bac69bf661721a3ca5fae5407c8cd9913f5f3bd
eldreco <andreffcd@gmail.com>
2023-04-25 15:06:13 +0100

    Added HTTP to item service

--- projetoPSI-angular/src/app/item.service.ts
+++ projetoPSI-angular/src/app/item.service.ts
@@ -2,20 +2,54 @@ import { Injectable } from '@angular/core';
 import { Item } from './item';
 import { ITEMS } from './mock-items';
 import { Observable, of } from 'rxjs';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { catchError, map, tap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ItemService {
-  getItem(id: number) : Observable<Item> {
-    const item = ITEMS.find(i => i.id === id)!;
-    return of(item);
+
+  private itemsUrl = 'api/items';  // URL to web api
+
+
+
+  httpOptions = {
+    headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+  };
+  
+  constructor(private http: HttpClient) { }
+
+  /** GET hero by id. Will 404 if id not found */
+  getItem(id: number): Observable<Item> {
+    const url = `${this.itemsUrl}/${id}`;
+    return this.http.get<Item>(url).pipe(
+      catchError(this.handleError<Item>(`getItem id=${id}`))
+    );
+  }
+  
+  getItems(): Observable<Item[]> {
+    return this.http.get<Item[]>(this.itemsUrl)
+    .pipe(
+      catchError(this.handleError<Item[]>('getItems', []))
+    );
   }
 
-  constructor() { }
+  /**
+   * Handle Http operation that failed.
+   * Let the app continue.
+   *
+   * @param operation - name of the operation that failed
+   * @param result - optional value to return as the observable result
+   */
+  private handleError<T>(operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
 
-  getItems(): Observable<Item[]> {
-    const items = of(ITEMS);
-    return items;
+      // TODO: send the error to remote logging infrastructure
+      console.error(error); // log to console instead
+
+      // Let the app keep running by returning an empty result.
+      return of(result as T);
+    };
   }
 }
