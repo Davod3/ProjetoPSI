e0e3535663af1bbd040eda567adf6d2d0953b76e
jaojao <uni.joaoassis@gmail.com>
2023-04-30 19:14:41 +0100

    Login working

--- projetoPSI-express/auth_config/passport.js
+++ projetoPSI-express/auth_config/passport.js
@@ -25,12 +25,4 @@ passport.use(new LocalStrategy(function(userid, password, done) {
 
 }));
 
-passport.serializeUser(function(user, done) {
-    done(null, user.id);
-  });
-  
-  passport.deserializeUser(function(id, done) {
-    User.findById(id, function(err, user) {
-      done(err, user);
-    });
-  });
+
