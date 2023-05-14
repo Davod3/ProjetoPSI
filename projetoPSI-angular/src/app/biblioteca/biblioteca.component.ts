5bd7989768ad23e7442038de11a149688fe046b7
WaveProtector <ricardojnmateuss@gmail.com>
2023-05-14 05:52:24 +0100

    US14 working; Falta css e ordenação por datas

--- projetoPSI-angular/src/app/biblioteca/biblioteca.component.ts
+++ projetoPSI-angular/src/app/biblioteca/biblioteca.component.ts
@@ -11,62 +11,47 @@ import { Location } from '@angular/common';
 })
 export class BibliotecaComponent implements OnInit {
 
-  items: Item[] = [];
+  library: Map<Item, string> = new Map<Item, string>();
+  items: [Item, string][] = [];
   ascendingOrder = true;
 
   constructor(
-
     private userService: UserService,
     private route: ActivatedRoute,
     private location: Location
-    
-    ){}
+  ) {}
 
   ngOnInit(): void {
-    
-      this.buildPage();
-      this.sortItems();
-
+    this.buildPage();
   }
 
   private buildPage(): void {
-    
     const id = String(this.route.snapshot.paramMap.get('id'));
-  
-    this.userService.getUserLibrary(id)
-      .subscribe(library => {
-          
-        this.items = library;
-  
-        });
-      
-    }
 
-    toggleSortOrder(): void {
+    this.userService.getUserLibrary(id).subscribe(library => {
+      this.library = library;
+      this.items = Array.from(library.entries());
+    });
+  }
 
-      this.ascendingOrder = !this.ascendingOrder;
-      this.sortItems();
+  toggleSortOrder(): void {
+    this.ascendingOrder = !this.ascendingOrder;
+    this.sortItems();
+  }
 
-    }
+  sortItems(): void {
+    this.items.sort((a, b) => {
+      const nameA = a[0].name.toLowerCase();
+      const nameB = b[0].name.toLowerCase();
+      if (this.ascendingOrder) {
+        return nameA.localeCompare(nameB);
+      } else {
+        return nameB.localeCompare(nameA);
+      }
+    });
+  }
 
-    sortItems(): void {
-
-      this.items.sort((a, b) => {
-
-        const nameA = a.name.toLowerCase();
-        const nameB = b.name.toLowerCase();
-        if (this.ascendingOrder) {
-          return nameA.localeCompare(nameB);
-        } else {
-          return nameB.localeCompare(nameA);
-        }
-        
-      });
-
-    }
-
-    goBack(): void {
-      this.location.back();
-    }
-
+  goBack(): void {
+    this.location.back();
+  }
 }
